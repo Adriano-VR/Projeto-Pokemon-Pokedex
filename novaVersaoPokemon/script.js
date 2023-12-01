693f87e01c587ca316c084fbc254b2db388b0fa9
Adriano2607 <rodrigues.adriano2607@gmail.com>
2023-12-01 02:51:26 -0300

    teste flip

--- novaVersaoPokemon/script.js
+++ novaVersaoPokemon/script.js
@@ -50,6 +50,71 @@
     section.appendChild(divContainer);
 }
 
+
+function teste(poke,pagina){
+    const section = document.getElementById('principal')
+
+    const divflipCard = document.createElement('div')
+    divflipCard.classList = 'flip-card'
+
+    divflipCard.id = `pokemon-${poke.id}`;
+    
+    const divflipFront = document.createElement('div')
+    divflipFront.classList = 'flip-card-front'
+
+    const divflipBack = document.createElement('div')
+    divflipBack.classList = 'flip-card-back'
+
+    const divflipinner = document.createElement('div')
+    divflipinner.classList = 'flip-card-inner'
+
+
+  
+
+    const divimagem = document.createElement('div')
+    divimagem.classList = 'imagem-tipos'
+
+    divimagem.innerHTML += `<img src='${poke.imgFrente}'></img>`;
+
+
+  
+
+    const divFilhaContent_content_tipos = document.createElement('div');
+    divFilhaContent_content_tipos.classList = 'content-tipos';
+
+    const ul = document.createElement('ul');
+
+    // verficar se e array
+    const tipos = Array.isArray(poke.tipo) ? poke.tipo : [poke.tipo];
+
+    tipos.forEach(type => {
+        const li = document.createElement('li');
+        li.textContent = type;
+        li.classList.add(`tipo-${type}`);
+        ul.appendChild(li);
+    });
+
+    divFilhaContent_content_tipos.appendChild(ul);
+
+    divflipFront.appendChild(divimagem)
+
+    divimagem.appendChild(divFilhaContent_content_tipos)
+
+    const divtexto = document.createElement('div')
+    divtexto.classList = 'nome'
+
+    divtexto.innerHTML = `<h1>${poke.nome}</h1>`
+    divflipFront.appendChild(divtexto)
+
+    divtexto.style.backgroundColor = corPorTipo(poke).corBackground
+   
+   
+    divflipinner.appendChild(divflipFront)
+    divflipinner.appendChild(divflipBack)
+    divflipCard.appendChild(divflipinner)
+ 
+    section.appendChild(divflipCard)
+}
 //Funcao separada para escolher a cor do fundo baseada no tipo do pokemon
 
 function corPorTipo(poke){
